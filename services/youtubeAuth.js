6b78a2e2536fa7b8c88111164b12a348ddd76f60
0sipov <smozoluk@gmail.com>
2025-04-16 19:14:18 +0300

    add YouTube OAuth2 client and API wrapper

---
+++ services/youtubeAuth.js
@@ -0,0 +1,15 @@
+const { google } = require('googleapis');
+require('dotenv').config();
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.YOUTUBE_CLIENT_ID,
+  process.env.YOUTUBE_CLIENT_SECRET,
+  process.env.YOUTUBE_REDIRECT_URI
+);
+
+const youtube = google.youtube({
+  version: 'v3',
+  auth: oauth2Client,
+});
+
+module.exports = { oauth2Client, youtube };
